42432f371c2ab5d1629e53107617b034e86a2389
Hyunjae Song <shj941013@yahoo.com>
2018-04-02 19:38:57 -0400

    test cases

--- clientApp/src/app/services/api/batch-controller/batch-controller.service.spec.ts
+++ clientApp/src/app/services/api/batch-controller/batch-controller.service.spec.ts
@@ -1,19 +1,57 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async, getTestBed } from '@angular/core/testing';
 
 import { BatchControllerService } from './batch-controller.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { batches } from '../../../mockdb/mockdata/batch.data';
+import { Observable } from 'rxjs/Observable';
+import { Batch } from '../../../model/batch';
+// import { from } from 'rxjs/Observable/from';
+import 'rxjs/add/observable/from';
 
 describe('BatchControllerService', () => {
+  let injector: TestBed;
+  let service: BatchControllerService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BatchControllerService, HttpClient, HttpHandler]
+      imports: [HttpClientTestingModule],
+      providers: [BatchControllerService, HttpClientTestingModule]
     });
+
+    injector = getTestBed();
+    service = injector.get(BatchControllerService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it(
-    'should be created',
-    inject([BatchControllerService], (service: BatchControllerService) => {
-      expect(service).toBeTruthy();
-    })
-  );
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('gettingBatches', () => {
+    it(
+      'should return an Observable<batches[]> from createBatch',
+      inject([BatchControllerService], (batch: BatchControllerService) => {
+        const b = batches;
+        spyOn(batch, 'createBatch').and.returnValues(Observable.of(b));
+        batch.createBatch(b[0] as Batch).subscribe(result => {
+          expect(result).toBeTruthy();
+          expect(result[0].id).toBe(187);
+        });
+      })
+    );
+
+    it(
+      'should return an Observable<batches[]> from deleteBatch',
+      inject([BatchControllerService], (batch: BatchControllerService) => {
+        const b = batches;
+        spyOn(batch, 'deleteBatch').and.returnValues(Observable.of(b));
+        batch.deleteBatch(187).subscribe(res => {
+          expect(res).toBeTruthy();
+          expect(res[0].id).toBe(187);
+        });
+      })
+    );
+  });
 });
